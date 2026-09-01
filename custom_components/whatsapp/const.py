c0fa0cd2d66f016a4a944c387fae48d07fab7ca3
danfulton72 <42965092+danfulton72@users.noreply.github.com>
2026-09-01 09:51:23 +0100

    Add notify and session service constants

--- custom_components/whatsapp/const.py
+++ custom_components/whatsapp/const.py
@@ -10,6 +10,12 @@ MANUFACTURER = "WhatsApp HA"
 MODEL_API = "WWebJS REST API"
 MODEL_SESSION = "WhatsApp Web session"
 
+NOTIFY_DOMAIN = "notify"
+SERVICE_NOTIFY_SEND_MESSAGE = "whatsapp_send_message"
 SERVICE_SEARCH_CONTACTS = "search_contacts"
+SERVICE_SESSION_START = "session_start"
+SERVICE_SESSION_END = "session_end"
+
 ATTR_SESSION_ID = "session_id"
 ATTR_PATTERN = "pattern"
+ATTR_MEDIA_URL = "media_url"
